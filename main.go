9d5e10fa1d170bead4ec59f0f7a204f239c2bc66
Yahnis allegret <yahnis.allegret@yahoo.com>
2023-10-11 16:21:32 +0900

    start

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+// ------------------------------------------------------first one to calculate the Area of wo objects-------------------------------------------------------------//
+
+// import "fmt"
+
+// type triangle struct {
+// 	height float64
+// 	base   float64
+// }
+
+// type square struct {
+// 	sideLength float64
+// }
+
+// type shape interface {
+// 	getArea() float64
+// }
+
+// func (t triangle) getArea() float64 {
+// 	return 0.5 * t.base * t.height
+// }
+
+// func (sq square) getArea() float64 {
+// 	return sq.sideLength * sq.sideLength
+// }
+
+// func printArea(sh shape) {
+// 	fmt.Println(sh.getArea())
+
+// }
+
+// func main() {
+
+// 	t := triangle{height: 10, base: 10}
+// 	sq := square{sideLength: 10}
+
+// 	printArea(t)
+// 	printArea(sq)
+
+// }
